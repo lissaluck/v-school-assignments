b9190e5b706c02200f70d9c507b7032ad8f6a666
lissaluck <lissaluck@gmail.com>
2017-01-13 12:49:11 -0700

    Add refinements to Uptown Function file

--- warmups/uptown/uptown.js
+++ warmups/uptown/uptown.js
@@ -1,54 +1,62 @@
 var lyrics = ["This", "hit", "that", "ice", "cold", "Michelle", "Pfeiffer", "that", "white", "gold", "This", "one", "for", "them", "hood", "girls", "Them", "good", "girls", "straight", "masterpieces", "Stylin'", "whilen'", "livin'", "it", "up", "in", "the", "city", "Got", "Chucks", "on", "with", "Saint", "Laurent", "Gotta", "kiss", "myself", "I'm", "so", "pretty"];
 
 /* with join method */
-//function printArray(lyricArray) {
-//    console.log(lyricArray.join(" "));
-//}
-//
-function printArray(lyricArray) {
-    var lyricString = "";
-    for (var i = 0; i < lyricArray.length ; i++) {
-        lyricString += lyricArray[i] + " ";
+function printArray(lyricsArray) {
+    console.log(lyricsArray.join(" "));
+}
+
+function printArray(lyricsArray) {
+    var lyricsString = "";
+    for (var i = 0; i < lyricsArray.length ; i++) {
+        lyricsString += lyricsArray[i] + " ";
     }
-    console.log(lyricString);
+    console.log(lyricsString);
 }
 
 printArray(lyrics);
 
 /* with join method */
-//function reversePrint(lyricArray) {
-//    lyricArray.reverse();
-//    console.log(lyricArray.join(" "));
+//function reversePrint(lyricsArray) {
+//    console.log(lyricsArray.reverse().join(" "));
 //}
 //
 //reversePrint(lyrics);
 
-function reversePrint(lyricArray) {
-    var lyricString = "";
+function reversePrint(lyricsArray) {
+    var lyricsString = "";
     for (var i = lyrics.length - 1; i >= 0; i--) {
-        lyricString += lyricArray[i] + " ";
+        lyricsString += lyricsArray[i] + " ";
     }
-    console.log(lyricString);
+    console.log(lyricsString);
 }
 
 reversePrint(lyrics);
 
-function printEvens(lyricArray) {
-    var lyricString = "";
-    for (var i = 0; i <lyrics.length - 1; i += 2) {
-        lyricString += lyricArray[i] + " ";
+function printEvens(lyricsArray) {
+    var lyricsString = "";
+    for (var i = 0; i < lyrics.length; i += 2) {
+        lyricsString += lyricsArray[i] + " ";
     }
-    console.log(lyricString);
+    console.log(lyricsString);
 }
 
 printEvens(lyrics);
 
-function jumbleArray(lyricArray) {
-    var lyricString = "";
-    for (var i = 1; i < lyricArray.length + 1 ; i += 2) {
-        lyricString += lyricArray[i] + " " + lyricArray[i - 1] + " ";
+
+function jumbleArray(lyricsArray) {
+    var lyricsString = "";
+    if (lyricsArray.length % 2 === 0) {
+        for (var i = 0, j = 1; i < lyricsArray.length; i += 2, j += 2) {
+            lyricsString += lyricsArray[j] + " " + lyricsArray[i] + " ";
+        }
+    } else {
+        console.log("Lyris are odd");
+        for (var i = 0, j = 1; i < lyricsArray.length - 1; i += 2, j += 2) {
+            lyricsString += lyricsArray[j] + " " + lyricsArray[i] + " ";
+        }
+        lyricsString += lyricsArray[lyricsArray.length - 1];
     }
-    console.log(lyricString);
+    console.log(lyricsString);
 }
 
 jumbleArray(lyrics);
