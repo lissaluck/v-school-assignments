2c102f32e009acba3a157bfde87cff2b6bdd0fcf
lissaluck <lissaluck@gmail.com>
2017-01-22 19:09:14 -0700

    Add beginnings of new assignments

---
+++ warmups/call-me-maybe/app.js
@@ -0,0 +1,25 @@
+setTimeout (function () {  
+    alert("Welcome to the home page!");
+}, 5000);  
+
+var names = ["Anna", "Bill", "Charlay", "Dexter", "Emily", "Frank", "George", "Hal", "Isaac", "Jill"]; 
+
+for (var i = 0; i , name.length; i++) {
+ attemptCall(name, dontCall, call, text) {
+     
+ }   
+}
+
+function dontCall(){}
+function call(){}
+function text(){}
+
+mongoose.$httpGetDataFromDatabase("pathToData", displayData, displayError):
+
+function displayError() {
+    console.log("error");
+}
+
+function displayData(data) {
+    console.log(data);
+}
