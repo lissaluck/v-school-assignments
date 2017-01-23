db4d9ff4edeb46cc75e7f168c33c4b204bb5f291
lissaluck <lissaluck@gmail.com>
2017-01-22 19:32:15 -0700

    Notes file removed

--- warmups/uptown/uptown.js
+++ warmups/uptown/uptown.js
@@ -1,9 +1,9 @@
 var lyrics = ["This", "hit", "that", "ice", "cold", "Michelle", "Pfeiffer", "that", "white", "gold", "This", "one", "for", "them", "hood", "girls", "Them", "good", "girls", "straight", "masterpieces", "Stylin'", "whilen'", "livin'", "it", "up", "in", "the", "city", "Got", "Chucks", "on", "with", "Saint", "Laurent", "Gotta", "kiss", "myself", "I'm", "so", "pretty"];
 
 /* with join method */
-function printArray(lyricsArray) {
-    console.log(lyricsArray.join(" "));
-}
+// function printArray(lyricsArray) {
+//     console.log(lyricsArray.join(" "));
+// }
 
 function printArray(lyricsArray) {
     var lyricsString = "";
