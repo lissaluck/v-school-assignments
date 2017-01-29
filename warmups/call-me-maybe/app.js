44caad3da3d89a76f3ac21d263055efc0df02b86
lissaluck <lissaluck@gmail.com>
2017-01-28 17:10:58 -0700

    Add callbacks warmup

--- warmups/call-me-maybe/app.js
+++ warmups/call-me-maybe/app.js
@@ -1,25 +1,42 @@
-setTimeout (function () {  
-    alert("Welcome to the home page!");
-}, 5000);  
+var names = ["Bruce", "Gail", "Eric", "Bryceton", "Ella", "MaryKay", "Sharon", "Debbie", "Mikey", "Andrea"];
 
-var names = ["Anna", "Bill", "Charlay", "Dexter", "Emily", "Frank", "George", "Hal", "Isaac", "Jill"]; 
-
-for (var i = 0; i , name.length; i++) {
- attemptCall(name, dontCall, call, text) {
-     
- }   
+for (var i = 0; i < names.length; i++) {
+    attemptCall(names[i], dontCall, call, sendText);
 }
 
-function dontCall(){}
-function call(){}
-function text(){}
-
-mongoose.$httpGetDataFromDatabase("pathToData", displayData, displayError):
-
-function displayError() {
-    console.log("error");
+function attemptCall(name, dontCall, call, sendText) {
+    nameLower = name.toLowerCase();
+    // If there is more than one "a" in the name
+    if (nameLower.lastIndexOf("a") !== nameLower.indexOf("a")) {
+        sendText(name);
+    } else if (nameLower.length % 2 !== 0) {
+        dontCall(name); 
+    } else {
+        call(name);
+    }
 }
 
-function displayData(data) {
-    console.log(data);
+function sendText(name) {
+    console.log(`Sent a text to ${name}`);
 }
+
+function dontCall(name) {
+    console.log(`Didn't call ${name}`);
+}
+
+function call(name) {
+    console.log(`Called ${name}`);
+}
+
+// execute function attemptCall for each name in array
+
+//attemptCall(name cb1, cb2, cb3) // could be this because I choose the name of parameter
+
+// these are the exercise parameter names
+//attemptCall(name, dontCallm call, sendText)
+// if name has more than one 'a'
+// sendText();
+// else if name.length is odd
+// dontCall()
+//else length is even
+// call();
