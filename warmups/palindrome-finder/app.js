f3d68a60acbd7c18766bc69311437cbe89ee40d9
lissaluck <lissaluck@gmail.com>
2017-01-31 21:45:50 -0700

    Add plaindrom finder program

--- warmups/palindrome-finder/app.js
+++ warmups/palindrome-finder/app.js
@@ -1,10 +1,9 @@
 function isPalindrome(phrase) {
-    phrase = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()" "]/g, "");
-    console.log(phrase);
-    phrase = phrase.toLowerCase;
+    phrase = phrase.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\"'\.\?" "]/g, "");
+//    console.log(phrase);
 
     for (var i = 0, j = phrase.length - 1; i < j; i++, j--) {
-        if (string[i] !== string[j]) {
+        if (phrase[i] !== phrase[j]) {
             return false;
         }
     }
@@ -14,19 +13,5 @@ function isPalindrome(phrase) {
 console.log(isPalindrome("Star Rats!"));
 console.log(isPalindrome("palindrome"));
 console.log(isPalindrome("eye"));
-
-
-//function isPalindrome(string) {
-//    string = string.toLowerCase().replace(/[^a-z]/g, "");
-//    for (var i = 0, j = string.length - 1; i < j; i++, j--) {
-//        if (string[i] !== string[j]) {
-//            return false;
-//        }
-//    }
-//    return true;
-//}
-//
-////var string = "I madam, I made radio! So I dared! Am I mad?? Am I?!";
-//var string = "Do good? I? No! Evil anon I deliver. I maim nine more hero-men in Saginaw, sanitary sword a-tuck, Carol, I- Lo! - rack, cut a drowsy rat in Aswan. I gas nine more hero-men in Miami. Reviled, I (Nona) live on. I do, O God!"
-//
-//console.log(isPalindrome(string));
+console.log(isPalindrome("Go hang a salami, I'm a lasagna hog"));
+console.log(isPalindrome("Do good? I? No! Evil anon I deliver. I maim nine more hero-men in Saginaw, sanitary sword a-tuck, Carol, I- Lo! - rack, cut a drowsy rat in Aswan. I gas nine more hero-men in Miami. Reviled, I (Nona) live on. I do, O God!"));
