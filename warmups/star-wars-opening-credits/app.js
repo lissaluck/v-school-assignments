4ee3d546bcc7d130002bd496f5939599b7b169e7
lissaluck <lissaluck@gmail.com>
2017-03-27 11:31:29 -0600

    Initial commit

---
+++ warmups/star-wars-opening-credits/app.js
@@ -0,0 +1,9 @@
+angular.module("starWarsCredits", [])
+
+.controller("MainController", ["$scope", "$http", function ($scope, $http) {
+
+    $http.get("http://swapi.co/api/films/1/")
+        .then(function (response) {
+            $scope.crawl = response.data.opening_crawl;
+    });
+}]);
