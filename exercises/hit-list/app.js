7f17d3d7c7a5827ac5a81d82b707ad873f36f3f0
lissaluck <lissaluck@gmail.com>
2017-03-27 15:58:34 -0600

    Removed unused code and cleaned up files and syntax

--- exercises/hit-list/app.js
+++ exercises/hit-list/app.js
@@ -1,26 +1,10 @@
-angular.module("HitListApp", [])
+angular.module("hitListApp", [])
 
 .controller("HitListController", ["$scope", "$http", function ($scope, $http) {
     
-//    $http({
-//        url: "http://api.vschool.io:6543/hitlist.json",
-//    }).then(function(response) {
-//        $scope.hitlist = response.data;
-//    })
     $http.get('http://api.vschool.io:6543/hitlist.json')
         .then(function (response) {
-//            console.log(response.data);
             $scope.hitlist = response.data;
             console.log($scope.name);
         });
 }]);
-
-//var req = {
-//    data: {
-//        name: 'name',
-//        image: 'image'
-//    }
-//}
-//
-//$http(req).then(function (response) {
-//            console.log(response.data);
