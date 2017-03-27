8b053652d0b9c3a3c438ab53455dc32ef753b1f4
lissaluck <lissaluck@gmail.com>
2017-03-27 10:26:34 -0600

    Added fibonacci sequence, quadratic, and finding frequent character functions

--- exercises/functions/functions.js
+++ exercises/functions/functions.js
@@ -1,7 +1,7 @@
 function sum(num1, num2) {
     return (num1 + num2);
 }
-            
+
 //console.log(sum(2, 3));
 
 function largest(num1, num2, num3) {
@@ -48,35 +48,60 @@ function stringOutput(str) {
 //console.log(stringOutput("I am a single mom to two beautiful kids."));
 
 function fibonacci(num) {
-    
+    var last = num;
+    for (var n = 0; n <= num; n++) {
+        var fib = 0;
+        var negn = 0 - n;
+        var phi = ((1 + Math.sqrt(5)) / 2);
+        var negphi = 0 - phi;
+        fib = Math.round(((Math.pow(phi, n)) - (Math.pow(negphi, negn))) / Math.sqrt(5));
+        console.log(fib);
+    }
+
+    //
+    //        while (i <= num) {
+    //            ((1 + math.pow(phi, n)) - (-math.pow(phi, i))) / ((Math.pow(2, n)) * Math.sqrt(5))
+    //        }
+    //
+    //
+    //        phi to the power of n - (-phi to the power of - n)
+    //        divided by the sqrt of 5
 }
+
+//fibonacci(5);
+//fibonacci(10);
 
 function quadratic(a, b, c) {
-    
+    var x1 = -b / 2 / a + Math.pow(Math.pow(b, 2) - 4 * a * c, 0.5) / 2 / a;
+    var x2 = -b / 2 / a - Math.pow(Math.pow(b, 2) - 4 * a * c, 0.5) / 2 / a;
+    console.log(x1);
+    console.log(x2);
+    //if (form.x1.value == "NaN") form.x1.value="Imag.!";
+    //if (form.x2.value == "NaN") form.x2.value="Imag.!";
 }
 
-function frequentChar(str) {
-    // count the number of times a character occurs
-    // compare the counts to find the largest
-    
+//quadratic(1, 12, 32);
+
+function findMostFrequent(string) {
+    var chars = {},
+        first = string.charAt(0);
+    chars[first] = 1;
+    var maxChar = first,
+        maxCount = 1;
+
+    for (var i = 1; i < string.length; i++) {
+        var char = string.charAt(i);
+        if (chars[char]) {
+            chars[char]++;
+        } else {
+            chars[char] = 1;
+        }
+        if (chars[char] > maxCount) {
+            maxChar = char;
+        }
+    }
+    console.log(maxChar);
 }
-//
-//function noDupes(str) {
-//    str = str.toLowerCase();
-//    var uniques = "";
-//    var misfits = "";
-//    for (var i = 0; i < str.length; i++) {
-//        if (uniques.indexOf(str[i]) !== -1) {
-//            misfits += str[i];
-//        } else {
-//            uniques += str[i];
-//        }
-//    }
-//    console.log("Uniques: " + uniques);
-//    console.log("Extras: " + misfits);
-//    
-//    var arr = uniques.split("");
-//    console.log(arr.sort().join(""));
-//}
-//
-//noDupes("bookkeeper Larry");
+
+
+findMostFrequent("The quick brown fox jumps over the lazy dog");
