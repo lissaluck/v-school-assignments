0f0ed38de6722f15d0c806423d529dc17f84a824
lissaluck <lissaluck@gmail.com>
2017-03-27 16:57:31 -0600

    Fixing spaces

--- exercises/yoda-speak-service/app.js
+++ exercises/yoda-speak-service/app.js
@@ -1,9 +1,9 @@
 var app = angular.module("myApp", []);
 
-app.controller("myController", ["$scope", "$http", "yodifyService", function($scope, $http, yodifyService) {
+app.controller("MyController", ["$scope", "$http", "yodifyService", function($scope, $http, yodifyService) {
     $scope.submitPhrase = function(phrase) {
         yodifyService.getPhrase(phrase).then(function(response) {
             $scope.yodaPhrase = response.data
         })
     }
-}])
+}]);
